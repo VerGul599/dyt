ddfa98d3b6226b77cdbcf861af4efa43a3b7e7fc
Вероника <@WIN-BBNSI5FE1UN>
2024-03-11 23:28:30 +0500

    Добавьте файлы проекта.

---
+++ dyt/dyt.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int A;
+    std::cout << "Введите целое число A: ";
+    std::cin >> A;
+
+    std::cout << "Числа B, для которых A делится на B*B без остатка и не делится на B*B*B без остатка: ";
+
+    for (int B = 1; B * B <= A; B++) 
+    {
+        if (A % (B * B) == 0 && A % (B * B * B) != 0) 
+        {
+            std::cout << B << " ";
+        }
+    }
+
+    return 0;
+}
